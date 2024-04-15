4d789af3879eff46dda2ad12f4637875198a3ddd
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2024-04-15 07:29:26 +0700

    Update

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -41,6 +41,110 @@ mouse_bindings = {
 	},
 }
 
+local process_icons = {
+	["docker"] = wezterm.nerdfonts.linux_docker,
+	["docker-compose"] = wezterm.nerdfonts.linux_docker,
+	["psql"] = "󱤢",
+	["usql"] = "󱤢",
+	["kuberlr"] = wezterm.nerdfonts.linux_docker,
+	["ssh"] = wezterm.nerdfonts.fa_exchange,
+	["ssh-add"] = wezterm.nerdfonts.fa_exchange,
+	["kubectl"] = wezterm.nerdfonts.linux_docker,
+	["stern"] = wezterm.nerdfonts.linux_docker,
+	["nvim"] = wezterm.nerdfonts.custom_vim,
+	["make"] = wezterm.nerdfonts.seti_makefile,
+	["vim"] = wezterm.nerdfonts.dev_vim,
+	["node"] = wezterm.nerdfonts.mdi_hexagon,
+	["go"] = wezterm.nerdfonts.seti_go,
+	["python3"] = "",
+	["zsh"] = wezterm.nerdfonts.dev_terminal,
+	["bash"] = wezterm.nerdfonts.cod_terminal_bash,
+	["btm"] = wezterm.nerdfonts.mdi_chart_donut_variant,
+	["htop"] = wezterm.nerdfonts.mdi_chart_donut_variant,
+	["cargo"] = wezterm.nerdfonts.dev_rust,
+	["sudo"] = wezterm.nerdfonts.fa_hashtag,
+	["lazydocker"] = wezterm.nerdfonts.linux_docker,
+	["git"] = wezterm.nerdfonts.dev_git,
+	["lua"] = wezterm.nerdfonts.seti_lua,
+	["wget"] = wezterm.nerdfonts.mdi_arrow_down_box,
+	["curl"] = wezterm.nerdfonts.mdi_flattr,
+	["gh"] = wezterm.nerdfonts.dev_github_badge,
+	["ruby"] = wezterm.nerdfonts.cod_ruby,
+}
+
+local function get_current_working_dir(tab)
+	local current_dir = tab.active_pane and tab.active_pane.current_working_dir or { file_path = "" }
+	local HOME_DIR = os.getenv("HOME")
+
+	return current_dir.file_path == HOME_DIR and "~" or string.gsub(current_dir.file_path, "(.*[/\\])(.*)", "%2")
+end
+
+function firstToUpper(str)
+	return (str:gsub("^%l", string.upper))
+end
+
+local function isempty(s)
+	return s == nil or s == ""
+end
+
+local function get_process(tab)
+	if not tab.active_pane or tab.active_pane.foreground_process_name == "" then
+		return nil
+	end
+
+	local process_name = string.gsub(tab.active_pane.foreground_process_name, "(.*[/\\])(.*)", "%2")
+	if string.find(process_name, "kubectl") then
+		process_name = "kubectl"
+	end
+
+	local process_icon = process_icons[process_name]
+
+	local MAX_STRING_LENGTH = 6
+	if not process_icon then
+		if string.len(process_name) > MAX_STRING_LENGTH then
+			process_name = string.sub(process_name, 1, MAX_STRING_LENGTH) .. "..."
+		end
+		return string.format("%s", firstToUpper(process_name))
+	end
+
+	return process_icons[process_name]
+end
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+	local has_unseen_output = false
+	if not tab.is_active then
+		for _, pane in ipairs(tab.panes) do
+			if pane.has_unseen_output then
+				has_unseen_output = true
+				break
+			end
+		end
+	end
+
+	local cwd = wezterm.format({
+		{ Text = get_current_working_dir(tab) },
+	})
+
+	if isempty(cwd) then
+		cwd = "./"
+	end
+
+	local process = get_process(tab)
+
+	local title = process and string.format(" %s (%s)  ", process, cwd) or " [?] "
+
+	if has_unseen_output then
+		return {
+			{ Foreground = { Color = "#28719c" } },
+			{ Text = title },
+		}
+	end
+
+	return {
+		{ Text = title },
+	}
+end)
+
 --- Default config settings
 config.scrollback_lines = 7000
 config.hyperlink_rules = wezterm.default_hyperlink_rules()
