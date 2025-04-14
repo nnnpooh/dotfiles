3f6fa6bcc473444cc647c690a6f105da2a65adc9
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2025-04-15 06:39:58 +0700

    Update

--- powershell_profile.ps1
+++ powershell_profile.ps1
@@ -1,9 +1,15 @@
+# oh-my-posh
 oh-my-posh init pwsh --config "$env:LOCALAPPDATA\Programs\oh-my-posh\themes\atomic.omp.json" | Invoke-Expression
+
+# Zoxide
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
+
+# Admin
 function GoAdmin { Start-Process pwsh –Verb RunAs }
+
+# PSFzf
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
 
-
-
-
-
+# PSReadLine
+Set-PSReadLineOption -PredictionViewStyle ListView
--- windows.ps1
+++ windows.ps1
@@ -4,28 +4,68 @@ Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine
 # Choco
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-# Powershell Core
-choco install powershell-core --version 7.4.3
+# Scoop (no admin)
+irm get.scoop.sh | iex # irm == invoke web request, ie download this file. iex == invoke expression, ie, evaluate the command.
 
-# ----- On Powershell Core
-choco install googlechrome --ignore-checksums
+# Powershell Core
+choco install -y powershell-core 
+
+# Tools
+choco install -y git ripgrep wget fd unzip gzip mingw make fzf bat gh zoxide
+
+# Install PSFzf
+Install-Module -Name PSFzf -y
+
+# PSReadLine
+Install-Module -Name PSReadLine -y
 
 # Create powershell profile
 New-Item -Path $PROFILE -Type File -Force
+copy-item powershell_profile.ps1 $profile
 
 # Oh-my-posh
 choco install oh-my-posh
 $env:Path += ";$env:LOCALAPPDATA\Programs\oh-my-posh\bin"
+oh-my-posh font install meslo
 
-# zoxide and fzf (activation script is included in powershell profile)
-choco install zoxide
-choco install fzf
-
-# NVIM
-choco install -y neovim git ripgrep wget fd unzip gzip mingw make
-choco install nvm
+# Node
 nvm install 21
 nvm use 21
+
+# Google Chrome
+choco install googlechrome --ignore-checksums -y
+
+# Application (add -y to skip confirmation)
+choco install vscode
+# If the prompt in VSCode integrated terminal displays weird ANSI escape code, see this solution: https://github.com/microsoft/vscode/issues/211922#issuecomment-2162554431
+
+choco install docker-desktop -y
+choco install golang -y
+choco install python312 -y
+choco install winrar -y
+choco install onecommander -y
+choco install screenpresso -y
+choco install dbeaver -y
+choco install obs-studio -y
+choco install postman -y
+choco install sumatrapdf.install -y
+choco install pdf24 -y
+choco install ccleaner -y
+choco install line -y
+choco install zoom -y
+choco install inkscape -y
+choco install googledrive -y
+choco install notepadplusplus.install -y
+choco install treesizefree -y
+choco install vlc.install -y
+choco install gimp -y
+choco install discord -y
+choco install mendeley-reference-manager -y 
+choco install traffic-monitor -y
+choco install joplin -y
+choco install obsidian -y
+choco install treesizefree -y
+choco install syncbackfree -y
 
 # Fonts
 choco install nerd-fonts-hack
@@ -34,51 +74,14 @@ choco install nerd-fonts-jetbrainsmono
 # Wezterm
 choco install wezterm
 
-# Nvim
+# NVIM
+choco install nvm
 cd $env:USERPROFILE/appdata/local
 git clone git@github.com:nnnpooh/kickstart.nvim.git nvim
 cd nvim
 git checkout -t origin/nr 
 
-# Application (add -y to skip confirmation)
-choco install vscode
-# If the prompt in VSCode integrated terminal displays weird ANSI escape code, see this solution: https://github.com/microsoft/vscode/issues/211922#issuecomment-2162554431
-
-choco install docker-desktop
-choco install golang
-choco install python311
-# Github CLI
-choco install gh
-choco install winrar
-# choco install freecommander-xe.install --ignore-checksums
-choco install onecommander
-choco install screenpresso
-choco install dbeaver
-choco install obs-studio
-choco install postman
-choco install sumatrapdf.install
-choco install pdf24
-choco install ccleaner
-choco install line
-choco install zoom
-choco install inkscape
-choco install googledrive
-choco install notepadplusplus.install
-choco install treesizefree
-choco install vlc.install
-choco install gimp
-choco install discord
-choco install mendeley-reference-manager
-choco install traffic-monitor
-choco install joplin
-choco install obsidian
-choco install treesizefree
-choco install syncbackfree
 # App not working 
 # one drive
 # team
-#
-# Install PSFzf
-Set-ExecutionPolicy RemoteSigned -Scope CurrentUser # Optional: Needed to run a remote script the first time
-irm get.scoop.sh | iex # irm == invoke web request, ie download this file. iex == invoke expression, ie, evaluate the command.
-Install-Module -Name PSFzf
+
