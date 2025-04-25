f11552b549ca7922550d5443abb26856b8c975c7
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2025-04-25 19:39:29 +0700

    Update

--- Windows/wsl.sh
+++ Windows/wsl.sh
@@ -5,3 +5,13 @@ oh-my-posh font install meslo
 
 #Put this in .profile
 eval "$(oh-my-posh init bash --config $HOME/.cache/oh-my-posh/themes/catppuccin.omp.json)"
+
+# Github CLI
+(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+        && out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+        && cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt update \
+	&& sudo apt install gh -y
