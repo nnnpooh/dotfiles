f3a67d0a8f55d3885634b744a6394a91d005691c
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2026-04-08 12:09:33 +0700

    Update

--- Linux/debian.sh
+++ Linux/debian.sh
@@ -1,18 +1,25 @@
 # !/bin/bash
 
-# Install zsh and ohmyzsh
-sudo apt update && sudo apt install zsh -y
+# Install
+sudo apt update && sudo apt upgrade -y
+sudo apt install unzip git curl zoxide 
+
+# Install fzf
+sudo apt install zsh -y
 zsh --version
 chsh -s $(which zsh)
+
+# Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sudo apt install curl git -y
+
 # Install zsh plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-source .zshrc
 
 # Install fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
+# Add zsh-autosuggestions zsh-syntax-highlighting zoxide to plugins in .zshrc
+sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting zoxide)/g' ~/.zshrc
 source .zshrc
